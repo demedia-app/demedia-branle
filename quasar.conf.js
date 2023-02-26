5dc0665c3bafdabdf5c72e7aa3cb87884f319934
Sithum Bopitiya <sithumsandeepap@gmail.com>
2023-02-26 18:35:22 +0530

    Get local relay form env

--- quasar.conf.js
+++ quasar.conf.js
@@ -99,6 +99,7 @@ module.exports = configure(function (ctx) {
         AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
         AWS_REGION: process.env.AWS_REGION,
         AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
+        LOCAL_RELAY: process.env.LOCAL_RELAY,
       }
     },
 
